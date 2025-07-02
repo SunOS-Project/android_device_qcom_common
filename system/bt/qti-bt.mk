8ed89969b7c4e61c4b0e1f2e9fa49599e96eada8
Abhay Singh Gill <abhaygill017@gmail.com>
2025-07-02 19:43:52 +0530

    system: bt: Update path for QSSI 16
    Change-Id: I16222b025cbad0492696742b3ac58797e3bb5507
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/bt/qti-bt.mk
+++ system/bt/qti-bt.mk
@@ -1,7 +1,7 @@
 # Include QTI Bluetooth makefiles.
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
-$(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
+include vendor/qcom/opensource/commonsys/bluetooth/bt-system-qssi-board.mk
+$(call inherit-product, vendor/qcom/opensource/commonsys/bluetooth/bt-system-opensource-product.mk)
 endif
 
 # Properties
